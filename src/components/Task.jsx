b66f51533dc8f96da9e2b38fcd5a602c56b8ab05
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2026-04-26 01:50:16 +0200

    Added IsTaskFinished handling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 export function Task({ taskDecription, priority }) {
+  const [isTaskFinished, setIsTaskFinished] = useState(false);
+  const buttonColorClasses = isTaskFinished
+    ? "bg-orange-500 hover:bg-orange-700"
+    : "bg-green-500 hover:bg-green-700";
+
   return (
     <div className="border rounded-xl my-4 hover:bg-gray-200 transition flex justify-between items-center p-4">
-      <span>{taskDecription}</span>
+      <span className={isTaskFinished ? "line-through" : ""}>
+        {taskDecription}
+      </span>
       <div className="flex gap-2 ">
         <button
           type="button"
-          className="bg-green-500 rounded-xl p-2 hover:bg-green-700 transition"
+          className={`${buttonColorClasses} rounded-xl p-2 transition`}
+          onClick={() => setIsTaskFinished((prev) => !prev)}
         >
-          Zrobione
+          {isTaskFinished ? "Do zrobienia" : "Zrobione"}
         </button>
         <button
           type="button"
